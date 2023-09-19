a7a737426588bba49718b1c99ca77d9b6c68304b
vfkami <viniciusqquei@gmail.com>
2023-09-18 21:14:44 -0300

    Project setup first commit

---
+++ src/main/kotlin/com/vfkami/gymAPI/GymApiApplication.kt
@@ -0,0 +1,11 @@
+package com.vfkami.gymAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<GymApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/vfkami/gymAPI/GymApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vfkami.gymAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GymApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
